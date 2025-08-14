918591cb5bef628a5a996b842df8c52923c32ce2
Aman Palod <palod.aman@gmail.com>
2025-08-13 22:53:49 -0700

    wishlist functionaly validates user_logged_in

--- app/javascript/controllers/wishlist_controller.js
+++ app/javascript/controllers/wishlist_controller.js
@@ -3,6 +3,15 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 
     updateWishlistStatus() {
+        //get status is user is logged in
+        //if logged out, redirect to login page, return
+        //else continue
+        const isUserLoggedIn = this.element.dataset.userLoggedIn;
+        if (isUserLoggedIn === "false") {
+            document.querySelector(".js-login").click(); //clicks eleement withthe class js-login which is the login button
+            return
+        }
+
         // for toggling, get data status. default status is fault, so unliked property would be false and empty at first
         //the status is a string even tho true or false
         if (this.element.dataset.status === "false") {
